a7b49d882144e3e68e27312df31ae261dabfee2a
Sarah Withee <github@geekygirlsarah.com>
2019-02-01 02:50:01 +0000

    Add example skill 3

---
+++ example-skill-3/README.md
@@ -0,0 +1,22 @@
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/door-closed.svg' card_color='#861189' width='50' height='50' style='vertical-align:bottom'/>
+ Knock Knock
+Tell Mycroft a knock knock joke!
+
+## About 
+This skill will let you tell Mycroft a knock knock joke. It will respond to you in a way that will let you do the full joke.
+
+## Examples 
+* "knock knock"
+
+## Credits 
+@geekygirlsarah
+Sarah Withee
+See https://geekygirlsarah.com/hey-mycroft for information on how the code works and to see the notes on how to make your own!
+
+
+## Category
+**Entertainment**
+WARNING: You have no Categories selected. Please ensure your Skill is assigned to at least one Category.
+
+## Tags
+
---
+++ example-skill-3/__init__.py
@@ -0,0 +1,34 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import getLogger
+
+__author__ = 'geekygirlsarah/Sarah Withee'
+LOGGER = getLogger(__name__)
+
+class KnockKnockSkill(MycroftSkill):
+
+    def get_user_response(self, dialog):
+        response = self.get_response(dialog)
+        return response
+
+    @intent_handler(IntentBuilder("").require("knock.knock"))
+    def handle_knock_knock_intent(self, message):
+        # They said "knock knock"
+
+        # You say "who's there?"
+        # Get the next line
+        noun = self.get_user_response("whos.there")
+
+        # They said noun
+        # You say "noun who?"
+        self.speak(noun)
+        self.get_user_response("who")
+
+        # They said a punchline
+        # You laugh in return
+        self.speak_dialog("laugh")
+
+def create_skill():
+    return KnockKnockSkill()
+
+
